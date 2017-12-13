173067dbd3458ec4c170ad5fc1a7fb80961f244e
Remus Knowles <remknowles@gmail.com>
2017-12-13 12:16:10 +0000

    day 12 part 1, continued explorations in poor graph algorithms.

--- 12/day12.py
+++ 12/day12.py
@@ -17,27 +17,36 @@ def parse():
 
 	return edges
 
-def main():
-	edges = parse()
+def merge_graphs(graphs,edges):
+	"""
+	Take a list of graphs and edges and do one pass of consolidation.
+	"""
 
-	# Start traversing edges and building connected sets.
-
-	subgraphs = [set([x]) for x in range(1,2000)]
+	new_graphs = []
 
 	for s,d in edges:
-		
-
-		for g1 in subgraphs:
-			for g2 in subgraphs:
+		for g1 in graphs:
+			for g2 in graphs:
 				if not g1 == g2 and s in g1 and d in g2:
-						print g1, g2
-						subgraphs_new = subgraphs
+					new_graphs.append(g1.union(g2))
 
-						subgraphs_new.append(g1.union(g2))
-						subgraphs_new.remove(g1)
-						subgraphs_new.remove(g2)
+	for g in graphs:
+		for ng in new_graphs:
+			if g.intersect(ng):
+				break
+		else:
+			new_graphs.append(g)
 
-	print subgraphs
+	return new_graphs
+
+def main():
+	edges = parse()
+	graphs = [set([x]) for x in range(2000)]
+	
+	for _ in range(1):
+		graphs = merge_graphs(graphs,edges)
+
+	print graphs
 
 if __name__ == '__main__':
 	main()
