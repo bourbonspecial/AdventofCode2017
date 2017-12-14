0a33dd44a9f0f27ab276bc2cc0d7bb1db55bfe86
Remus Knowles <remknowles@gmail.com>
2017-12-14 09:13:31 +0000

    day 10 part 1, got myself all in a muddle before starting again with a really basic solution. Much simpler if you rotate the list then rotate back again.

--- 10/day10.py
+++ 10/day10.py
@@ -3,24 +3,25 @@
 __author__ = 'Remus Knowles <remknowles@gmail.com>'
 
 inp = [206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3]
-inp = [3, 4, 1, 5]
 
 def main():
 	start = range(256)
-	start = range(5)
 	skip = 0
 	pt = 0
+
 	for el in inp:
-		rng_start = pt + skip
-		rng_end = pt + skip + el
+		# Rotate list
+		rstart = [start[(i+pt) % len(start)] for i in range(len(start))]
 
-		# reverse the sublist
-		subl = [start[i % len(start)] for i in range(rng_start, rng_end + 1)][::-1]
+		new_start = rstart[:el][::-1] + rstart[el:]
 
-		start = 
+		# Rotate the list back again
+		start = [new_start[(i-pt) % len(new_start)] for i in range(len(new_start))]
 
-		skip += 1 
+		pt += el + skip
+		skip += 1
 
+	print start[0] * start[1]
 
 if __name__ == '__main__':
 	main()
