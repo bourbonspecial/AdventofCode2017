50c308a6268ab5fda4a53d72a6eac603829c0310
Remus Knowles <remknowles@gmail.com>
2017-12-15 09:40:44 +0000

    day 12 part 2, more brute forcing. Had to adjust some params to make sure each graph was properly constructed.

--- 12/day12.py
+++ 12/day12.py
@@ -2,6 +2,8 @@
 
 __author__ = 'Remus Knowles <remknowles@gmail.com>'
 
+from datetime import datetime
+
 def parse():
 	"""
 	Take the input and turn it in to a list of edges.
@@ -18,16 +20,31 @@ def parse():
 	return edges
 
 def main():
+	start = datetime.now()
 	edges = parse()
+
+	groups_upper_bound = 2000
+	group_construct_iters = 30
 	g = set([0])
 
-	for _ in xrange(10):
-		for s, d in edges:
-			if s in g or d in g:
-				g.add(s)
-				g.add(d)
+	groups = []
 
-	print len(g)
+	for i in range(groups_upper_bound):
+		g = set([i])
+		for _ in xrange(group_construct_iters):
+			for s, d in edges:
+				if s in g or d in g:
+					g.add(s)
+					g.add(d)
+		groups.append(g)
+
+	clean_groups = []
+	for g in groups:
+		if not g in clean_groups:
+			clean_groups.append(g)
+
+	print datetime.now() - start
+	print len(clean_groups)
 
 if __name__ == '__main__':
 	main()
